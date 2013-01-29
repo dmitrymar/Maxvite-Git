466ad4da777126a4fc7df6aca8a348321d9f9933
dmitry mardukhayev <dmitrymar@yahoo.com>
2013-01-29 15:44:54 -0500

    added common.js before modifing

---
+++ js/common.js
@@ -0,0 +1,10 @@
+var globals = { 
+ spinner : "<img class='loading' src='/img/spinner.gif' alt='loading...' />" 
+} 
+function popUp(url, width, height) {
+	var page = url;
+	var width = width;
+	var height = height;
+	var windowprops = "toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width="+width+", height="+height+"";
+	window.open(page, 'PopupName', windowprops);
+}
